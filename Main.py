8b5862ef03ca0661671f394c3aaa33cb98166d32
elmanto <mantovaniale452@gmail.com>
2017-10-28 12:35:50 +0200

    Generazione grafo per nodi a più dimensioni di quasi id

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from Nodo import Nodo
 
 
 generalizzazione = dict()
-generalizzazione['zipcode'] = 5
+generalizzazione['zipcode'] = 3
 generalizzazione['sesso'] = 2
 generalizzazione['data'] = 3
 numeroQuasiIdentifier = 3
@@ -10,6 +10,7 @@ numeroQuasiIdentifier = 3
 
 def stampo(root):
     i = 0
+    print(root)
     while(len(root.nextNode) > 0):
         node = root.nextNode.pop()
         print("Nodo successivo esistente !!!!" + str(i) + " ")
@@ -20,35 +21,41 @@ def stampo(root):
 Esegue la query e inserisce tutti gli zipcode (etc...) all'interno del primo nodo
 '''
 
-def costruisciGrafoRicorsivo(nodo, i, quasiIdIndex):
+def costruisciGrafoRicorsivo(nodo, generValue, quasiIdIndex):
 
-    a = nodo.levelOfGeneralizations[nodo.quasiIdentifier[quasiIdIndex]]
-    b = generalizzazione[nodo.quasiIdentifier[quasiIdIndex]]
-    print("valore di a -> " + str(a))
-    print("valore di b -> " + str(b))
-    if(a == b):
-        return
-    nodo.levelOfGeneralizations[nodo.quasiIdentifier[quasiIdIndex]] = i
-    nodoSucc = Nodo(nodo.quasiIdentifier, False)
-    nodo.nextNode.append(nodoSucc)
-    costruisciGrafoRicorsivo(nodoSucc, i+1, quasiIdIndex)
+    for i in range(0, len(nodo.quasiIdentifier)):
+        livelloDiGeneralizzazioneCorrente = nodo.levelOfGeneralizations[nodo.quasiIdentifier[quasiIdIndex]]
+        livelloDiGeneralizzazioneMax = generalizzazione[nodo.quasiIdentifier[quasiIdIndex]]
+        print("valore di a -> " + str(livelloDiGeneralizzazioneCorrente))
+        print("valore di b -> " + str(livelloDiGeneralizzazioneMax))
+        if(livelloDiGeneralizzazioneCorrente < livelloDiGeneralizzazioneMax):
+            nodo.levelOfGeneralizations[nodo.quasiIdentifier[quasiIdIndex]] = generValue
+            nodoSucc = Nodo(nodo.quasiIdentifier, False)
+            nodo.nextNode.append(nodoSucc)
+            costruisciGrafoRicorsivo(nodoSucc, generValue + 1, quasiIdIndex)
+        elif(livelloDiGeneralizzazioneCorrente == livelloDiGeneralizzazioneMax and i < len(nodo.quasiIdentifier)):
+            continue
+        else:
+            return
+
 
 def costruisciGrafo(root, quasiIdentifiers, quasiIdIndex):
     root.isRoot = True
     root.quasiIdentifier = quasiIdentifiers
 
-    root.levelOfGeneralizations[quasiIdentifiers[quasiIdIndex]] = 0
-    nodo = Nodo(quasiIdentifiers, False)
-    print(nodo.quasiIdentifier[quasiIdIndex])
-    root.nextNode.append(nodo)
-    costruisciGrafoRicorsivo(nodo, 1, quasiIdIndex)
+    for i in range(0, len(quasiIdentifiers)):
+        root.levelOfGeneralizations[quasiIdentifiers[i]] = 0
+        nodo = Nodo(quasiIdentifiers, False)
+        print(nodo.quasiIdentifier[quasiIdIndex])
+        root.nextNode.append(nodo)
+        costruisciGrafoRicorsivo(nodo, 1, quasiIdIndex)
     return root
 
 
 quasiIdentifier = []
 quasiIdentifier.append("zipcode")
 quasiIdentifier.append("sesso")
-quasiIdentifier.append("data")
+
 
 # generiamo tutte le coppi di quasiIdenifier
 quasiIdentifierCouple = []
@@ -61,10 +68,10 @@ for qi in range(0, len(quasiIdentifier)):
 for i in quasiIdentifierCouple:
     print(i)
 
-for i in range (0, 3):
-    root = Nodo(quasiIdentifier, False)
-    root = costruisciGrafo(root, quasiIdentifier, i)
-    stampo(root)
+
+root = Nodo(quasiIdentifier, False)
+root = costruisciGrafo(root, quasiIdentifier, 0)
+stampo(root)
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Incognito
+
+ Python implementation of Incognito algorithm to reach k-anonimity of a database
